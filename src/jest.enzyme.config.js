56ac9d4efaa4d757d1e7076dbbee2986c30c420b
scott <scott.lan@innovasolutions.com>
2018-04-09 11:52:42 +0800

    update code

--- src/jest.enzyme.config.js
+++ src/jest.enzyme.config.js
@@ -3,3 +3,14 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+// for jasmine-reporter
+var jasmineReporters = require('jasmine-reporters');
+jasmine.VERBOSE = true;
+jasmine.getEnv().addReporter(
+    new jasmineReporters.JUnitXmlReporter({
+        consolidateAll: true,
+        savePath: './',
+        filePrefix: 'test-results'
+    })
+);
